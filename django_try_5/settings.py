849919bf703c787c08d38841d0fd0291253657eb
saurav <tessco913@gmail.com>
2024-02-16 15:06:46 +0530

    commit_6

--- django_try_5/settings.py
+++ django_try_5/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-*pesx@kkx+uhw_tyz(e2(+=go^6d75wnc1rna-#r8ehzbiaax_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['agilelms.onrender.com']
+ALLOWED_HOSTS = ['agilelms.onrender.com', '127.0.0.1']
 
 # Application definition
 
--- django_try_5/urls.py
+++ django_try_5/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("auth/", include("user_auth.urls")),
@@ -8,3 +9,6 @@ urlpatterns = [
     path("teacher/", include("teacher.urls")),
     path("student/", include("student.urls")),
 ]
+
+
+
